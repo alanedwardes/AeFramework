ab51c2c66209e03c598ebf271016ce5c7f12961d
Alan Edwardes <alan@alanedwardes.com>
2014-04-12 19:24:09 +0100

    Removed debug return in loader.php, removed Twig include.

--- loader.php
+++ loader.php
@@ -9,7 +9,6 @@ spl_autoload_register(function($name){
 	# If this class is in the AeFramework namespace
 	if (substr($name, 0, strlen(__NAMESPACE__)) == __NAMESPACE__)
 	{
-		return;
 		# Truncate the namespace, including the directory separator
 		$name = substr($name, strlen(__NAMESPACE__) + 1);
 		
@@ -17,6 +16,3 @@ spl_autoload_register(function($name){
 		require_once sprintf('src%s%s.php', DIRECTORY_SEPARATOR, $name);
 	}
 });
-
-# Vendor Specific
-require_once 'vendor/Twig.php';
