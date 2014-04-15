1289a8084184549bdd7672bd7b515884da95f791
Alan Edwardes <alan@alanedwardes.com>
2014-04-15 22:33:38 +0100

    Added another redirect test.

--- test/RedirectViewTestCase.php
+++ test/RedirectViewTestCase.php
@@ -6,5 +6,24 @@ class RedirectViewTestCase extends PHPUnit_Framework_TestCase
 		$redirect = new AeFramework\TemporaryRedirectView('http://www.example.com/');
 		
 		$this->assertSame($redirect->headers(), ['Location' => 'http://www.example.com/']);
+		$this->assertSame($redirect->code(), AeFramework\HttpCode::Found);
+	}
+	
+	public function testPermanentRedirectViewRedirects()
+	{
+		$redirect = new AeFramework\PermanentRedirectView('http://www.example.com/');
+		
+		$this->assertSame($redirect->headers(), ['Location' => 'http://www.example.com/']);
+		$this->assertSame($redirect->code(), AeFramework\HttpCode::MovedPermanently);
+	}
+	
+	public function testRedirectViewWithMapperParameters()
+	{
+		$redirect = new AeFramework\TemporaryRedirectView('http://www.example.com/posts/%s/');
+		
+		# Normally set by the mapper, but we are mocking here
+		$redirect->map(array('post_slug' => 'testing'));
+		
+		$this->assertSame($redirect->headers(), ['Location' => 'http://www.example.com/posts/testing/']);
 	}
 }
