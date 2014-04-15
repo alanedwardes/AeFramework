199767702f03d29a6145bbb9d7d2699d50321165
Alan Edwardes <alan@alanedwardes.com>
2014-04-15 01:29:37 +0100

    Added InlineFileView and DownloadFileView for serving inline files and attachments.

---
+++ src/DownloadFileView.php
@@ -0,0 +1,23 @@
+<?php
+namespace AeFramework;
+
+class DownloadFileView extends InlineFileView
+{
+	public $download_name;
+	
+	public function __construct($file, $content_type, $download_name = null)
+	{
+		# If the download_name is null, the download name becomes the file
+		# name without any directory names before it
+		$this->download_name = ($download_name == null ? basename($file) : $download_name);
+		
+		parent::__construct($file, $content_type);
+	}
+	
+	public function headers()
+	{
+		return array_merge([
+			new HttpHeader('Content-Disposition', sprintf('attachment; filename="%s"', $this->download_name))
+		], parent::headers());
+	}
+}
---
+++ src/InlineFileView.php
@@ -0,0 +1,38 @@
+<?php
+namespace AeFramework;
+
+class InlineFileView implements IView
+{
+	public $file;
+	public $content_type;
+	
+	public function __construct($file, $content_type)
+	{
+		$file_path = Util::joinPath(getcwd(), $file);
+		
+		if (!file_exists($file_path))
+			throw new \Exception(sprintf('File "%s" does not exist.', $file));
+		
+		$this->file = $file_path;
+		$this->content_type = $content_type;
+	}
+	
+	public function map($params = [])
+	{
+	}
+	
+	public function code()
+	{
+		return HttpCode::Ok;
+	}
+	
+	public function headers()
+	{
+		return [new HttpHeader('Content-Type', $this->content_type)];
+	}
+	
+	public function body()
+	{
+		return readfile($this->file);
+	}
+}
