856e7a2f6fcf165e33ef3f2b58816843c9a0c4bc
Alan Edwardes <alan@alanedwardes.com>
2014-04-16 02:21:56 +0100

    Added PHP APC cache layer.

---
+++ src/ApcCache.php
@@ -0,0 +1,15 @@
+<?php
+namespace AeFramework;
+
+class ApcCache extends Cache
+{
+	public function get($key)
+	{
+		return apc_fetch($key);
+	}
+	
+	public function set($key, $data, $expire = 0)
+	{
+		return apc_add($key, $data, $expire);
+	}
+}
