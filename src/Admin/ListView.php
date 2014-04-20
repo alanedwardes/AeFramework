2deca3a33a93d5a7d8f8255a66ec4938cf07d5f1
Alan Edwardes <alan@alanedwardes.com>
2014-04-21 00:08:08 +0100

    Added simple pagination.

--- src/Admin/ListView.php
+++ src/Admin/ListView.php
@@ -10,6 +10,31 @@ class ListView extends AdminView
 	
 	public function body()
 	{
-		return parent::body(['rows' => $this->da->select($this->table)]);
+		$user_order = @$_GET['order'];
+		$user_direction = @$_GET['direction'];
+		$user_limit = @$_GET['limit'];
+		$user_start = @$_GET['start'];
+		
+		$direction = ($user_direction == 'desc') ? 'desc' : 'asc';
+		$order = reset($this->table->columns)->name;
+		
+		foreach ($this->table->columns as $column)
+		{
+			if ($column->name == $user_order)
+			{
+				$order = $column->name;
+			}
+		}
+		
+		$limit = min(max(intval($user_limit), 20), 500);
+		$start = max(intval($user_start), 0);
+		
+		return parent::body([
+			'rows' => $this->da->select($this->table, "1 ORDER BY {$order} {$direction} LIMIT {$start}, {$limit}"),
+			'order' => $order,
+			'direction' => $direction,
+			'limit' => $limit,
+			'start' => $start,
+		]);
 	}
 }
