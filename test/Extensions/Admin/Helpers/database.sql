c471adb659a61419499e0782febf577946255088
Alan Edwardes <alan@alanedwardes.com>
2014-05-11 01:33:50 +0100

    Added foreign keys to database. These can't be used right now due to the Doctrine DBAL not supporting them.

--- test/Extensions/Admin/Helpers/database.sql
+++ test/Extensions/Admin/Helpers/database.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE store (
 	id       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	name     VARCHAR NOT NULL,
@@ -22,13 +24,30 @@ INSERT INTO product(id, name) VALUES(4, 'Crisps');
 INSERT INTO product(id, name) VALUES(5, 'Cola');
 INSERT INTO product(id, name) VALUES(6, 'Cornflakes');
 
+CREATE TABLE store_product (
+	id         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	store_id   INTEGER NOT NULL,
+	product_id INTEGER NOT NULL,
+	
+	CONSTRAINT store_product_store_id   FOREIGN KEY(store_id)   REFERENCES store(id)   ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT store_product_product_id FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO store_product(store_id, product_id) VALUES(1, 1);
+INSERT INTO store_product(store_id, product_id) VALUES(1, 2);
+INSERT INTO store_product(store_id, product_id) VALUES(1, 3);
+INSERT INTO store_product(store_id, product_id) VALUES(2, 3);
+INSERT INTO store_product(store_id, product_id) VALUES(2, 4);
+INSERT INTO store_product(store_id, product_id) VALUES(2, 5);
+INSERT INTO store_product(store_id, product_id) VALUES(3, 6);
+
 CREATE TABLE employee (
 	id         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	first_name VARCHAR NOT NULL,
 	surname    VARCHAR NOT NULL,
 	store_id   INTEGER NOT NULL,
 	
-	FOREIGN KEY(store_id) REFERENCES store(id)
+	CONSTRAINT employee_store_id FOREIGN KEY(store_id) REFERENCES store(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO employee(id, first_name, surname, store_id) VALUES(1, 'David', 'Birch', 1);
