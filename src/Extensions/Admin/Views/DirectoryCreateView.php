b1e21628c0fa7d891520c599ef00f457d96d8484
Alan Edwardes <alan@alanedwardes.com>
2014-07-02 18:19:59 +0100

    Added ability to create directories.

--- src/Extensions/Admin/Views/DirectoryCreateView.php
+++ src/Extensions/Admin/Views/DirectoryCreateView.php
@@ -4,5 +4,26 @@ use Carbo\Extensions\Admin\FolderInformation;
 
 class DirectoryCreateView extends DirectoryView
 {
+	private $item;
+	private $created = false;
 
+	public function request($verb, array $params = [])
+	{
+		parent::request($verb, $params);
+		
+		if ($verb == \Carbo\Http\Verb::Post)
+		{
+			mkdir($this->folder->path . DIRECTORY_SEPARATOR . basename($_POST['name']));
+			$this->headers['Location'] = '../' . basename($_POST['name']) . '/';
+			$this->created = true;
+		}
+	}
+
+	public function response()
+	{
+		if (!$this->created)
+		{
+			return parent::response(['item' => $this->item]);
+		}
+	}
 }
