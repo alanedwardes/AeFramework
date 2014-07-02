a3e71354a27fe5536e8868051b6303a651157230
Alan Edwardes <alan@alanedwardes.com>
2014-07-02 14:29:08 +0100

    Made a model not necessary for some admin views.

--- src/Extensions/Admin/AdminRouter.php
+++ src/Extensions/Admin/AdminRouter.php
@@ -24,18 +24,21 @@ class AdminRouter extends \Carbo\Routing\AuthenticatedRouter
 		if ($this->model_connection)
 		{
 			\Carbo\Mapping\Map::create($this, [
-				['', '\Carbo\Extensions\Admin\Views\IndexView', 'index.html', $template_dir, $this->model_connection],
-				['logout/', '\Carbo\Extensions\Admin\Views\LogoutView'],
 				['r^table/(?P<table>.*)/delete/(?P<key>.*)/(?P<value>.*)/$', '\Carbo\Extensions\Admin\Views\DeleteView', 'delete.html', $template_dir, $this->model_connection],
 				['r^table/(?P<table>.*)/blob/(?P<key>.*)/(?P<value>.*)/(?P<field>.*)/$', '\Carbo\Extensions\Admin\Views\BlobView', 'blob.html', $template_dir, $this->model_connection],
 				['r^table/(?P<table>.*)/edit/(?P<key>.*)/(?P<value>.*)/$', '\Carbo\Extensions\Admin\Views\EditView', 'edit.html', $template_dir, $this->model_connection],
 				['r^table/(?P<table>.*)/create/$', '\Carbo\Extensions\Admin\Views\CreateView', 'create.html', $template_dir, $this->model_connection],
 				['r^table/(?P<table>.*)/$', '\Carbo\Extensions\Admin\Views\ListView', 'list.html', $template_dir, $this->model_connection],
-				['r^directory/(?P<directory>.*)/create/$', '\Carbo\Extensions\Admin\Views\DirectoryCreateView', 'directory_create.html', $template_dir],
-				['r^directory/(?P<directory>.*)/upload/$', '\Carbo\Extensions\Admin\Views\DirectoryUploadView', 'directory_upload.html', $template_dir],
-				['r^directory/(?P<directory>.*)/$', '\Carbo\Extensions\Admin\Views\DirectoryListView', 'directory_list.html', $template_dir],
 			]);
 		}
+		
+		\Carbo\Mapping\Map::create($this, [
+			['', '\Carbo\Extensions\Admin\Views\IndexView', 'index.html', $template_dir, $this->model_connection],
+			['logout/', '\Carbo\Extensions\Admin\Views\LogoutView'],
+			['r^directory/(?P<directory>.*)/create/$', '\Carbo\Extensions\Admin\Views\DirectoryCreateView', 'directory_create.html', $template_dir],
+			['r^directory/(?P<directory>.*)/upload/$', '\Carbo\Extensions\Admin\Views\DirectoryUploadView', 'directory_upload.html', $template_dir],
+			['r^directory/(?P<directory>.*)/$', '\Carbo\Extensions\Admin\Views\DirectoryListView', 'directory_list.html', $template_dir],
+		]);
 		
 		$this->error(\Carbo\Http\Code::Forbidden, ['\Carbo\Extensions\Admin\Views\LoginView', 'login.html', $template_dir]);
 		
